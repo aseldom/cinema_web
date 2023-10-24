c6eeae5b2ded8c6d5fd7283eeb70337034dbc205
aseldom <a.seldom@gmail.com>
2023-10-24 15:17:13 +0600

    liquibase tables create

---
+++ db/scripts/011_dml_insert_files.sql
@@ -0,0 +1,5 @@
+insert into files(name, path) values
+    ('001.jpg', 'files\001.jpg'),
+    ('002.jpg', 'files\002.jpg'),
+    ('003.jpg', 'files\003.jpg'),
+    ('004.jpg', 'files\004.jpg');
---
+++ db/scripts/041_dml_insert_halls.sql
@@ -0,0 +1,4 @@
+insert into halls(name, row_count, place_count, description) values
+    ('Big', 20, 20, 'Большой зал'),
+    ('Midle', 15, 15, 'Средний зал'),
+    ('Small', 10, 10, 'Малый зал');
